7fb1a5cc789662bf72df2d64e32dd3b8edb6210a
Haydar Tuna <haydartuna@hotmail.com>
2021-12-20 11:49:09 +0300

    Stack fonksiyon eklendi

--- Stack/stackLinkedList.cpp
+++ Stack/stackLinkedList.cpp
@@ -15,6 +15,10 @@ int pop(Stack **stack);
 int peek( Stack *stack );
 void printStack(Stack *stack);
 bool isEmpty( Stack *stack );
+int sum(Stack *top);
+int sumRec(Stack *top);
+int length(Stack *top);
+int lengthRec(Stack *top);
 
 int main() {
     Stack* top = nullptr;
@@ -26,8 +30,10 @@ int main() {
     push(&top, 5);
 
     printStack(top);
-    cout << endl << peek( top ) << endl;
-
+    cout << "Toplam: " << sum( top ) << endl;
+    cout << "Toplam: " << sumRec( top ) << endl;
+    cout << "Uzunluk: " << length( top ) << endl;
+    cout << "Uzunluk: " << lengthRec( top ) << endl;
 
     return 0;
 }
@@ -119,3 +125,65 @@ bool isEmpty( Stack *stack )
     return !stack;
 }
 
+//Stack içindeki değerlerin toplanması
+int sum(Stack *top)
+{
+    //Stack içinde dolaşmak için gerekli pointer
+    Stack *temp = top;
+
+    int sum = 0;
+    while (temp != nullptr)
+    {
+        //Düğüm değeri toplandı
+        sum += temp->data;
+
+        //Yıgındaki bir sonraki değere geç
+        temp = temp->next;
+    }
+
+    return sum;
+}
+
+//Stack içindeki değerlerin toplanması
+int sumRec(Stack *top)
+{
+    if ( top != nullptr ) {
+        return top->data + sumRec(top->next);
+    }
+    else 
+    {
+        return 0;
+    }
+}
+
+
+//Stack uzunlugu
+int length(Stack *top)
+{
+    //Stack içinde dolaşmak için gerekli pointer
+    Stack *temp = top;
+
+    int length = 0;
+    while (temp != nullptr)
+    {
+        //Uzunluk artırıldı
+        length ++;
+
+        //Yıgındaki bir sonraki değere geç
+        temp = temp->next;
+    }
+
+    return length;
+}
+
+//Stack uzunlugu
+int lengthRec(Stack *top)
+{
+    if ( top != nullptr ) {
+        return 1 + lengthRec(top->next);
+    }
+    else 
+    {
+        return 0;
+    }
+}
